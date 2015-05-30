d5cea84120c505f11e01206c29ecb7fce3796679
TwoBit <robert.walenta@alumni.fh-aachen.de>
2015-05-30 19:55:11 +0200

    Version 0.0.1
    - Added gitignore file
    - Added timer.h + timer.c
    - Added state_machine_main.h + state_machine_main.c
    - Added main.c
    
    Implementations:
    
    - Timer0 was programmed for a Time-Slicer, generated frequencies for 1Hz,
      10Hz, 100Hz, 1kHz and 10kHz
    
    To Do:
    - Main Statemachine has to be implemented and the viso documentation
      has to be done for it
    
    Signed-off-by: TwoBit <robert.walenta@alumni.fh-aachen.de>

---
+++ include/state_machine_main.h
@@ -0,0 +1,16 @@
+/*
+ * state_machine_main.h
+ *
+ * Created: 30.05.2015 19:50:38
+ *  Author: TwoBit
+ */ 
+
+
+#ifndef STATE_MACHINE_MAIN_H_
+#define STATE_MACHINE_MAIN_H_
+
+
+
+
+
+#endif /* STATE_MACHINE_MAIN_H_ */
---
+++ src/state_machine_main.c
@@ -0,0 +1,8 @@
+/*
+ * state_machine_main.c
+ *
+ * Created: 30.05.2015 19:50:53
+ *  Author: TwoBit
+ */ 
+
+#include "../include/state_machine_main.h"
