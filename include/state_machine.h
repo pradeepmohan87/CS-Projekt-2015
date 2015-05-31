c7a13128b32d0d7c2a533c33db864ac70f9d2840
TwoBit <robert.walenta@alumni.fh-aachen.de>
2015-05-31 17:32:46 +0200

    Version 0.0.1
    Added a Satemachine for debouncing buttons that will be used for
    controlling the microcontroller. First idea is to use joystick buttons
    up,down,left and right for navigation in the menus and to have some kind
    of an confim and cancel button.
    
    So in the end 6 buttons has to be unbounced to make sure only one pressing
    of a button is recognized.
    
    Signed-off-by: TwoBit <robert.walenta@alumni.fh-aachen.de>

--- include/state_machine.h
+++ include/state_machine.h
@@ -9,8 +9,19 @@
 #ifndef STATE_MACHINE_MAIN_H_
 #define STATE_MACHINE_MAIN_H_
 
+#include <avr/io.h>
 
+void SM_MAIN(void);					// 10kHz
+void SM_PRINT(void);				// 1kHz
+void SM_DEBOUNCE(uint8_t pin);		// 100Hz 
+void SM_LCD(void);					// 10Hz
 
+enum BUTTON_STATES{
+	WAIT_FOR_BUTTON_ON = 0,
+	DEBOUNCING_BUTTON_ON = 50,
+	WAIT_FOR_BUTTON_OFF = 100,
+	DEBOUNCING_BUTTON_OFF =	159
+};
 
 
 #endif /* STATE_MACHINE_MAIN_H_ */
--- src/state_machine.c
+++ src/state_machine.c
@@ -6,3 +6,52 @@
  */ 
 
 #include "../include/state_machine.h"
+
+void SM_MAIN(void){
+	
+}	
+void SM_PRINT(void){
+	
+}				
+void SM_DEBOUNCE(uint8_t pin){
+	volatile static int state = WAIT_FOR_BUTTON_ON;
+	volatile static int counter = 0;
+	
+	switch(state)
+	{
+		case WAIT_FOR_BUTTON_ON: {
+			if((PINA & pin) == 0){
+				counter = 2;
+				state = DEBOUNCING_BUTTON_ON;
+			}
+			break;
+		}
+		case DEBOUNCING_BUTTON_ON: {
+			--counter;
+			if(counter == 0)
+			{
+				state = WAIT_FOR_BUTTON_OFF;
+			}
+			break;
+		}
+		case WAIT_FOR_BUTTON_OFF: {
+			if((PINA & pin) != 0)
+			{
+				counter = 2;
+				state = DEBOUNCING_BUTTON_OFF;
+			}
+			break;
+		}
+		case 150: {
+			--counter;
+			if(counter == 0)
+			{
+				state = 0;
+			}
+			break;
+		}
+	}	
+}		
+void SM_LCD(void){
+	
+}					
